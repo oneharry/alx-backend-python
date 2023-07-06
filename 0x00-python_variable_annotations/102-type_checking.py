d51bcc2bfa45c9401309fbadcf74bffae0f7c425
oneharry <harryofficialchidi@yahoo.com>
2023-07-06 23:37:04 +0000

    implemented task 12

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Validate type checking """
+from typing import Tuple, Union, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List[int] = [
+        item for item in lst
+        for _ in range(factor)
+    ]
+    return zoomed_in
+
+
+array: Tuple[int, int, int] = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)

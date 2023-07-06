72f0a251da7ceaa8217b9257479c9e30388cb3f0
oneharry <harryofficialchidi@yahoo.com>
2023-07-06 18:05:15 +0000

    Implemented adv task 10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Duck typing - first element of sewuence"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None

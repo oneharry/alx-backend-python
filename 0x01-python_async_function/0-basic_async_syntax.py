e40dbdf0de6bc1303b727fd68e58f0339e93ed28
oneharry <harryofficialchidi@yahoo.com>
2023-07-10 11:42:45 +0000

    made changes the conditional

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,4 +7,6 @@ import asyncio
 async def wait_random(max_delay: int = 10) -> float:
     """ Function waits for max_delay, returns the the value """
     await asyncio.sleep(max_delay)
+    if max_delay < 0:
+        return random.uniform(max_delay, 0)
     return random.uniform(0, max_delay)

66a4bedd1ababd7b69fd5b5505c503c4662cc788
oneharry <harryofficialchidi@yahoo.com>
2023-07-11 18:18:00 +0000

    pycodestyle errors

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Async Comprehension """
 import asyncio
-from typing import Iterable, Iterator
+from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Iterable[float]:
+async def async_comprehension() -> Generator[float]:
     """ Collect random number from func, returns random num"""
     return [item async for item in async_generator()]

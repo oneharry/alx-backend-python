97e559445eec466be0e4c7e59e6c6d3bd8ee25e4
Oneharry <harryofficialchidi@yahoo.com>
2023-07-11 18:04:59 +0100

    changed geenrator to Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ Async generator """
 import asyncio
 import random
-from typing import Iterable
+from typing import Generator
 
 
-async def async_generator() -> Iterable[float]:
+async def async_generator() -> Generator[float, None, None]:
     """ Loops 10 time, yield a random number after waiting 1 sec"""
     for i in range(10):
         await asyncio.sleep(1)

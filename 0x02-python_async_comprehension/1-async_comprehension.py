23b3a9cfe456a726daa60ba6c219650178e10ff6
Oneharry <harryofficialchidi@yahoo.com>
2023-07-31 09:33:42 +0100

    resolve git conflit

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/ebn python3
 """ Async Comprehension """
 import asyncio
-from typing import Iterable, Iterator
+from typing import Iterable
 async_generator = __import__('0-async_generator').async_generator
 
 
